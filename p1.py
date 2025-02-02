3fa52193a029573ffd46275f58f5fe0bb7a019cf
ChickenLitte <145792244+ChickenLitte@users.noreply.github.com>
2025-02-02 21:41:02 +0000

    v2

--- p1.py
+++ p1.py
@@ -1,2 +1,39 @@
 def group_anagrams(words):
-    pass
+    result = {}
+    for i in range(len(words)):
+        print(words[i],hasher(words[i]))
+        if hasher(words[i]) not in result.keys():
+            result.update({hasher(words[i]):[words[i]]})
+            print("new")
+        else:
+            result[hasher(words[i])].append(words[i])
+            print(hasher(words[i]),"old")
+    print(result)
+def hasher(word,mod = 13):
+    # combinedVal = 0
+    # for char in word:
+    #     combinedVal += ord(char)
+    # return combinedVal % mod
+    val = 0 
+    word = sort(word)
+    for i in range(0,len(word)):
+        val += ord(word[i]) * 31^i 
+    return val
+
+def sort(word):
+        word = list(word)
+        for i in range(0,len(word)):
+            smallestElement = i
+            for j in range(i,len(word)):
+                if word[j]<=word[smallestElement]:smallestElement = j#set the new smallest element
+            word[i],word[smallestElement] = word[smallestElement],word[i]#swap stuff
+        return ''.join(word)
+
+def main():
+    words = ["bat", "tab", "eat",  "tea", "tan", "nat"]
+    group_anagrams(words)
+if __name__ == "__main__":
+    main()
+# Given a list of strings, group the strings into lists of anagrams. Return the groups as a list of lists. For example, if the input is: ["bat", "meta", "tab", "tea", "ate", "tame", "jazz" "eat"], the output would be: [["bat", "tab"], ["meta", "tame"], ["tea", "ate", "eat"],["jazz"]]
+# If three words are anagrams, what common key could we store to represent them in a hash table?
+# To convert a list to a string, we can use `''.join(someList)` to concatenate the elements of the list to the empty string.
--- p2.py
+++ p2.py
@@ -1,2 +1,32 @@
 def k_most_frequent(lst, k):
-    pass
+    occurences = {}#Should I even be using a hashing FN?\
+    k = 0
+    kth = []
+    for i in range(len(lst)):
+        print(lst[i],hasher(lst[i]))
+        if hasher(lst[i]) not in occurences.keys():
+            occurences.update({hasher(lst[i]):[lst[i]]})
+            print("new")
+            if not k == 0:
+                kth.append(lst[i])
+                print('hi')
+        else:
+            occurences[hasher(lst[i])].append(lst[i])
+            print(hasher(lst[i]),"old")
+            if len(occurences[hasher(lst[i])]) >= k and lst[i] not in kth:
+                kth.append(lst[i])
+                print('sup')
+
+    print(occurences)
+    print(kth)
+    return kth
+def hasher(word):
+    return(hash(word))
+
+def main():
+    k_most_frequent([1, 1, 1, 2, 2, 3], 2)
+if __name__ == "__main__":
+    main()
+#Given a list and a value k, return the k most frequently occurring elements. 
+# If there is a tie for kth-most frequent, return all the tied elements. 
+# If k is greater than the numbe of unique elements, return a list of all the unique elements.
