a40b9ee947dca85de1b4e2a223ff6ec7b71a3d26
ChickenLitte <145792244+ChickenLitte@users.noreply.github.com>
2025-02-06 23:50:28 +0000

    v7, p1 fully working and simplified

--- p1.py
+++ p1.py
@@ -1,29 +1,13 @@
 def group_anagrams(words):
-    result = {}
-    for i in range(len(words)):
-        if hasher(words[i]) not in result.keys():
-            result.update({hasher(words[i]):[words[i]]})
+    groups = {}
+    for item in words:
+        key = "".join(sorted(item))
+        if key in groups:
+            groups[key].append(item)
         else:
-            result[hasher(words[i])].append(words[i])
-    final = []
-    for val in result.keys():
-        final.append(result[val]) 
-    print(final)
-    return final
-def hasher(word,mod = 13):
-    word = sort(word)
-    return hash(word)
-
-def sort(word):
-        word = list(word)
-        word.sort()
-        return ''.join(word)
-
+            groups[key] = [item]
+    return list(groups.values())
 def main():
-    words = ["bat", "tab", "eat", "tea", "tan", "nat"]
-    group_anagrams(words)
+    group_anagrams(["bat"])#[["bat", "tab"], ["eat", "tea"], ["tan", "nat"]]
 if __name__ == "__main__":
     main()
-# Given a list of strings, group the strings into lists of anagrams. Return the groups as a list of lists. For example, if the input is: ["bat", "meta", "tab", "tea", "ate", "tame", "jazz" "eat"], the output would be: [["bat", "tab"], ["meta", "tame"], ["tea", "ate", "eat"],["jazz"]]
-# If three words are anagrams, what common key could we store to represent them in a hash table?
-# To convert a list to a string, we can use `''.join(someList)` to concatenate the elements of the list to the empty string.
