8ff7e5fcb6d5006e02fc8205c1d716a274ece899
ChickenLitte <145792244+ChickenLitte@users.noreply.github.com>
2025-02-06 20:20:31 +0000

    v4

--- p3.py
+++ p3.py
@@ -1,5 +1,23 @@
 def count_subarrays_with_sum(arr, sum):
-    pass
+    if len(arr) == 0 : return 0
+    counts = {}
+
+    initial_sum = {0:1}
+    result = 0
+    current_sum = 0
+
+    for val in arr:
+        current_sum += val
+        if current_sum - sum in initial_sum:
+            result += initial_sum[current_sum - sum]
+        initial_sum[current_sum] = initial_sum.get(current_sum,0) + 1
+
+    return result
+
+def main():
+    print(count_subarrays_with_sum([], 0))
+if __name__ == "__main__":
+    main()
 #Given a list of integers and a target sum, 
 #return the number of continuous subarrays that sum to sum. 
 #For example, if the input is [1,1,1] and sum=2, the output should be 2.
